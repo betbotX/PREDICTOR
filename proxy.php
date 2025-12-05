00b0ee2a0ab66e454360a803134131ee13054d35
PREDICTOR <150495917+betbotX@users.noreply.github.com>
2025-12-05 12:52:04 +0100

    Delete proxy.php

--- proxy.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: application/json");
-
-$apiKey = "tl64J1XkhQGI7JXOs9kxpTxshTJAUqiHd558xJCD";
-
-$url = "https://api.sportradar.com/soccer/trial/v4/en/seasons/sr:season:118689/missing_players.json?api_key=" . $apiKey;
-
-echo file_get_contents($url);
