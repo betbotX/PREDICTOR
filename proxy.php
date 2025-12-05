a7e52e48f39774f8317c76aba0db914e10ac336c
PREDICTOR <150495917+betbotX@users.noreply.github.com>
2025-12-05 12:25:22 +0100

    Add files via upload

---
+++ proxy.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-Type: application/json");
+
+$apiKey = "tl64J1XkhQGI7JXOs9kxpTxshTJAUqiHd558xJCD";  // Mets ta clé ici, côté serveur c'est sécurisé
+
+$url = "https://api.sportradar.com/soccer/trial/v4/en/seasons/sr:season:118689/missing_players.json?api_key=" . $apiKey;
+
+echo file_get_contents($url);
